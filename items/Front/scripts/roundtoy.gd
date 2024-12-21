e2052ff9f47a7416042fe47a659db77a372f7b72
DoomFishy <calvinlu350@gmail.com>
2024-12-20 23:55:31 -0500

    Add files via upload

---
+++ items/Front/scripts/roundtoy.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+var shader_material
+
+func _ready():
+	shader_material = $Sprite2D.material
+
+func _on_mouse_entered():
+	shader_material.set_shader_parameter("line_thickness", 0.2)
+	
+func _on_mouse_exited():
+	shader_material.set_shader_parameter("line_thickness", 0)
